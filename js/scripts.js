0a58ed1a87548561a72801e5f1bf9aee1cabd3f3
PauloAlves8039 <pj38alves@gmail.com>
2022-02-07 20:47:11 -0300

    Obtendo o código do local com o Geocode API

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,35 @@
 $(function () {
+  var accuweatherAPIKey = "yZxRJ6ARJwGbAkussNJHylGLNE0ooE9G";
 
-  var accuweatherAPIKey = "yZxRJ6ARJwGbAkussNJHylGLNE0ooE9G";  
-
-  $.ajax({
-        url: "http://dataservice.accuweather.com/currentconditions/v1/45881?apikey=" + accuweatherAPIKey + "&language=pt-br",
-        type: "GET",
-        dataType: "json",
-        success: function (data) {
+  function pegarTempoAtual(localCode) {
+    $.ajax({
+      url: "http://dataservice.accuweather.com/currentconditions/v1/" + localCode + "?apikey=" + accuweatherAPIKey + "&language=pt-br",
+      type: "GET",
+      dataType: "json",
+      success: function (data) {
         console.log(data);
-        },
-        error: function () {
+      },
+      error: function () {
         console.log("Erro");
-        },
-  });
+      },
+    });
+  }
+
+  function pegarLocalUsuario(lat, long) {
+    $.ajax({
+      url: "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + accuweatherAPIKey + "&q=" + lat + "%2C" + long + "&language=pt-br",
+      type: "GET",
+      dataType: "json",
+      success: function (data) {
+        var localCode = data.Key;
+        pegarTempoAtual(localCode);
+      },
+      error: function () {
+        console.log("Erro");
+      },
+    });
+  }
+
+  pegarLocalUsuario(-8.055393255494286, -34.88415317276026);
+
 });
